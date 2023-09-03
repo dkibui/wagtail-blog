a4a31b14d1ec9a0be4b1cacd9b1aa5bcc63ac3b4
David Kibui <david@regulusweb.com>
2023-09-03 22:22:08 +0300

    Added BlogTagIndexPage model and template

--- blog/models.py
+++ blog/models.py
@@ -29,17 +29,27 @@ class BlogIndexPage(Page):
 
 class BlogPageTag(TaggedItemBase):
     content_object = ParentalKey(
-        'BlogPage',
-        related_name='tagged_items',
-        on_delete=models.CASCADE
+        "BlogPage", related_name="tagged_items", on_delete=models.CASCADE
     )
+
+
+class BlogTagIndexPage(Page):
+    def get_context(self, request):
+        # Filter by tag
+        tag = request.GET.get("tag")
+        blogpages = BlogPage.objects.filter(tags__name=tag)
+
+        # Update template context
+        context = super().get_context(request)
+        context["blogpages"] = blogpages
+        return context
 
 
 class BlogPage(Page):
     date = models.DateField("Post date")
     intro = models.CharField(max_length=250)
     body = RichTextField(blank=True)
-    authors = ParentalManyToManyField('blog.Author', blank=True)
+    authors = ParentalManyToManyField("blog.Author", blank=True)
     tags = ClusterTaggableManager(through=BlogPageTag, blank=True)
 
     def main_image(self):
@@ -55,14 +65,17 @@ class BlogPage(Page):
     ]
 
     content_panels = Page.content_panels + [
-        MultiFieldPanel([
-            FieldPanel('date'),
-            FieldPanel('authors', widget=forms.CheckboxSelectMultiple),
-            FieldPanel('tags'),
-        ], heading="Blog information"),
-        InlinePanel('gallery_images', label="Gallery images"),
-        FieldPanel('intro'),
-        FieldPanel('body'),
+        MultiFieldPanel(
+            [
+                FieldPanel("date"),
+                FieldPanel("authors", widget=forms.CheckboxSelectMultiple),
+                FieldPanel("tags"),
+            ],
+            heading="Blog information",
+        ),
+        InlinePanel("gallery_images", label="Gallery images"),
+        FieldPanel("intro"),
+        FieldPanel("body"),
     ]
 
 
